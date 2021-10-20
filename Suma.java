85ece31aadbd02c115122b8c458b545cf9eede39
misterminaya <misterminayape@gmail.com>
2021-10-20 11:19:11 -0500

    helloworld with java

---
+++ Suma.java
@@ -0,0 +1,12 @@
+public class Suma{
+  public static void main(String args[]){
+  
+    int number1 = 5;
+    int number2 = 2;
+    int resultado = 0; 
+    
+    resultado = number1 + number2;
+    System.out.println("El valor de tu suma es: " + resultado);
+ 
+ }
+}
